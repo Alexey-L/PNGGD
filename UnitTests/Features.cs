55aa1f6542c296e8aa654b82fdbe20b1fc8a58f4
SU\alexey.levin <alexey.levin@sigma.software>
2021-02-01 16:00:40 +0200

    UnitTest project was created

---
+++ UnitTests/Features.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitTests
+{
+    using System;
+
+    class Features
+    {
+        public int xSum(int x1, int x2)
+        {
+            return x1 + x2;
+        }
+
+        public int xSum(int x1, int x2, int x3)
+        {
+            return x1 + x2 + x3;
+        }
+
+        public int xSum(params int[] list)
+        {
+            int res = 0;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                res += list[i];
+            }
+
+            return res;
+        }
+
+        public string xSum(string x1, string x2)
+        {
+            return (Convert.ToInt32(x1) + Convert.ToInt32(x2)).ToString();
+        }
+    }
+}
---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,76 @@
+namespace UnitTests
+{
+    using NUnit.Framework;
+
+    public class UnitTest
+    {
+        [Test]
+        public void TestDoubleSum()
+        {
+            const int x1 = 6;
+            const int x2 = 7;
+            const int expRes = 13;
+
+            Features ef = new Features();
+            Assert.AreEqual(expRes, ef.xSum(x1, x2));
+        }
+
+        [Test]
+        public void TestMultipleSum1()
+        {
+            const int x1 = 5;
+            const int x2 = 5;
+            const int x3 = 5;
+            const int x4 = 5;
+            const int x5 = 5;
+            const int expRes = 25;
+
+            Features ef = new Features();
+            Assert.AreEqual(expRes, ef.xSum(x1, x2, x3, x4, x5), "‘се пропало");
+        }
+
+        [Test]
+        public void TestMultipleSum2()
+        {
+            const int x1 = 6;
+            const int expRes = 6;
+
+            Features ef = new Features();
+            Assert.AreEqual(expRes, ef.xSum(x1));
+        }
+
+        [Test]
+        public void TestTripleSum()
+        {
+            const int x1 = 6;
+            const int x2 = 7;
+            const int x3 = 2;
+            const int expRes = 15;
+
+            Features ef = new Features();
+            Assert.AreEqual(expRes, ef.xSum(x1, x2, x3));
+        }
+
+        [Test]
+        public void TestMultipleAssert()
+        {
+            Features ef = new Features();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(15.Equals(ef.xSum(5, 5, 5)), "Assert message #1");
+                Assert.That(100.Equals(ef.xSum(33, 33, 34)), "Assert message #2");
+                Assert.AreNotEqual(100.Equals(ef.xSum(33, 33, 34)), "Assert message #3");
+            });
+        }
+
+        [Test]
+        public void TestDoubleStringSum()
+        {
+            Features ef = new Features();
+
+            Assert.That(ef.xSum("10", "101"), Does.Not.Contain("0"), "Assert message #4");
+            Assert.That("Some Text", Does.Not.Contain("www"), "Assert message #5");
+        }
+    }
+}
